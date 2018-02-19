4bb9e8db2005efb137e3af02dc834b8e5f3cb635
erettozi <erettozi@gmail.com>
2018-02-19 13:32:45 -0300

    Update README.md and examples/bsTree.js

--- examples/bsTree.js
+++ examples/bsTree.js
@@ -6,6 +6,6 @@
 
 const BST = new (require('sbinlignum')).BST();
 
-BST.search(7, [1,2,5,7,9,10], (result) => {
+BST.search(7, [13, 3, 4, 12, 14, 10, 5, 1, 8, 2, 7, 9, 11, 6, 18], (result) => {
 	console.log(result);
 });
